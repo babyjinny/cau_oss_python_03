cf46560ce1f4a28646b0745cb53b563994d42935
babyjinny <114330740+babyjinny@users.noreply.github.com>
2023-03-23 22:21:38 +0900

    Update volume_calc.py
    총 길이에 따른 우체국 택배 요금 계산하기

--- volume_calc.py
+++ volume_calc.py
@@ -1,7 +1,28 @@
-Width = float(input("가로 : "))
-Length = float(input("세로 : "))
-Height = float(input("높이 : "))
+#박스 부피를 계산하여 우체국 택배 보내기
 
-Volume = Width * Length * Height
+#사용자에게 입력받기
+Width = float(input("width : "))
+Length = float(input("length : "))
+Height = float(input("height : "))
 
-print("박스의 부피는",Volume,"입니다.")
+#총 길이 계산
+total_length = Width + Length + Height
+
+#80cm 이하 5000원
+#80cm 초과 100cm 이하 8000원
+#100cm 초과 120cm 이하 10000원
+#120cm 초과 160cm 이하 13000원
+#160cm 초과 배송불가
+
+if total_length <= 80 :
+	money = 5000
+elif total_length <= 100 :
+	money = 8000
+elif total_length <= 120 :
+	money = 10000 
+elif total_length <= 160 :
+	money = 13000
+else :
+	money = "unavailable"
+
+print("Total Length : ",total_length,"money : ",money)
